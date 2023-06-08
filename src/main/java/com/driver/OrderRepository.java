855f1e7573e4f1b913cc67a41ce864ae66d2d566
Sahil Dhiman <dhiman.sahil03022001@gmail.com>
2023-06-08 12:09:42 +0530

    c2

--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Repository
 public class OrderRepository {
 
     HashMap<String, Order> orderDb = new HashMap<>();
--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+    OrderRepository orderRepository = new OrderRepository();
 
     public void addOrder(Order order) {
         orderRepository.addOrder(order);
